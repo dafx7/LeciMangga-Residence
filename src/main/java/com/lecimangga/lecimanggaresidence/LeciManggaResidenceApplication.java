54dc77ce2dfc44470873be03bde1af4071bdcd6d
Dapnek <ahmaddaffaamd@gmail.com>
2025-04-11 16:25:27 +0700

    Initial Commit

---
+++ src/main/java/com/lecimangga/lecimanggaresidence/LeciManggaResidenceApplication.java
@@ -0,0 +1,13 @@
+package com.lecimangga.lecimanggaresidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeciManggaResidenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeciManggaResidenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lecimangga/lecimanggaresidence/LeciManggaResidenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecimangga.lecimanggaresidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeciManggaResidenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
