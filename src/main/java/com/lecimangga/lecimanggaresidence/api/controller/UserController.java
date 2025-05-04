5359c2aa5c31cede73394c220a6593bbe4d563b2
Dapnek <ahmaddaffaamd@gmail.com>
2025-05-04 23:56:10 +0700

    [USER] menambahkan POST, UPDATE, dan DELETE ke model user

--- src/main/java/com/lecimangga/lecimanggaresidence/api/controller/UserController.java
+++ src/main/java/com/lecimangga/lecimanggaresidence/api/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,7 @@ public class UserController {
 
     @GetMapping("/test")
     public String test() {
-        return "niko kontol";
+        return "testing";
     }
 
     @GetMapping
@@ -35,5 +36,35 @@ public class UserController {
         }
 
         return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) throws SQLIntegrityConstraintViolationException {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return new ResponseEntity<String>("Duplicate Entry "+ user.getUsername(), HttpStatus.IM_USED);
+        }
+        userRepository.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        if (userRepository.findById(user.getId()) == null) {
+            return new ResponseEntity<String>("Unable to update as  User id " + user.getId() + " not found.",
+                    HttpStatus.NOT_FOUND);
+        }
+
+        userRepository.updateUser(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Integer id) {
+        User user = userRepository.findById(id);
+        if (user == null) {
+            return new ResponseEntity<String>("Unable to delete as  User id " + id + " not found.", HttpStatus.NOT_FOUND);
+        }
+        userRepository.deleteUserById(id);
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
     }
 }
