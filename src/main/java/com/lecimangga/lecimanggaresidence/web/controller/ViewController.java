bebdb1eefc6755c932f9c17ca2ffd203ffb1083b
Dapnek <ahmaddaffaamd@gmail.com>
2025-05-05 22:19:12 +0700

    [hubungi-kami] Fix height di navbar, menggantikan png arrow menjadi icon FontAwesome, membuat folder khusus web untuk server-side rendering template.

--- src/main/java/com/lecimangga/lecimanggaresidence/web/controller/ViewController.java
+++ src/main/java/com/lecimangga/lecimanggaresidence/web/controller/ViewController.java
@@ -1,7 +1,11 @@
-package com.lecimangga.lecimanggaresidence.api.controller;
+package com.lecimangga.lecimanggaresidence.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller untuk menangani permintaan halaman web (views).
+ */
 
 @Controller
 public class ViewController {
